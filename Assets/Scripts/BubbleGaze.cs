9044b554b675c08a0ade8a6b4867f28373056feb
Thomas Pfeiffer <thomas.pfeiffer@kde.org>
2020-09-03 21:52:40 +0200

    Rebuilt scene, added gaze pointer and script to color bubble (not working)
    Had to rebuild the scene because it showed up as empty from git.
    Tried adding Gaze Pointer and a script that changes the color of the bubble on gaze, but gaze does not work yet.

---
+++ Assets/Scripts/BubbleGaze.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleGaze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       var renderer = GetComponent<Renderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void GazeEnter()
+    {
+       GetComponent<Renderer>().material.color = Color.blue;
+    }
+    
+    public void GazeExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+        
+}
