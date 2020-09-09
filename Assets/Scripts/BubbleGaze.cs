907c1456bf41333ddc7a98a81a61484b54f2ad51
j13055 <jas.blekic@gmx.de>
2020-09-09 23:36:45 +0200

    Prefabs of gazing working version

--- Assets/Scripts/BubbleGaze.cs
+++ Assets/Scripts/BubbleGaze.cs
@@ -1,29 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+
 
 public class BubbleGaze : MonoBehaviour
 {
+    bool inside = false;
     // Start is called before the first frame update
     void Start()
     {
-       var renderer = GetComponent<Renderer>(); 
+       var renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
-    
+
     public void GazeEnter()
     {
-       GetComponent<Renderer>().material.color = Color.blue;
+        GetComponent<Renderer>().material.color = Color.blue;
+        inside = true;
     }
     
     public void GazeExit()
     {
         GetComponent<Renderer>().material.color = Color.white;
+        inside = false;
     }
         
 }
